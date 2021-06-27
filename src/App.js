e98e6909379a26eb9885ce4cec7f121c465a7731
Serhat Aksakal <hi@serhat.codes>
2021-06-27 20:39:37 +0300

    publish
    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { TextField } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import { useEffect, useState } from "react";
+const App = () => {
+  const locations = require("./city.list.min.json");
+  const [cities, setCities] = useState([]);
+  const [city, setCity] = useState();
+  const [weather, setWeather] = useState({});
 
-function App() {
+  useEffect(() => {
+    locations.map((l) => {
+      l.description = `${l.name.toUpperCase()}${
+        l.state ? `, ${l.state}` : ""
+      }, ${l.country}`;
+      return l;
+    });
+  }, []);
+
+  useEffect(() => {
+    if (city) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?id=${city.id}&units=metric&lang=tr&appid=ad0a5158454ba7e5d887ce71da06a747`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setWeather({
+            temperature: result.main.temp,
+            description: result.weather[0].description,
+            icon: result.weather[0].icon,
+          });
+        })
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+    }
+  }, [city]);
+
+  //
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Autocomplete
+        freeSolo
+        className="search"
+        options={cities}
+        onSelect={(e) => {
+          const value = e.target.value.toUpperCase();
+          if (value.length >= 3) {
+            const possibleLocations = locations
+              .filter((l) => l.description.includes(value))
+              .slice(0, 10);
+            setCities(possibleLocations.map((l) => l.description));
+          }
+          const selected = locations.find((l) => l.description === value);
+          setCity(selected);
+        }}
+        renderInput={(params) => (
+          <TextField {...params} placeholder="şehir ara" variant="outlined" />
+        )}
+      />
+      <div hidden={!weather.temperature}>
+        <div
+          className="temperature"
+          style={{ color: weather.temperature <= 0 ? "purple" : "orangered" }}
         >
-          Learn React
-        </a>
-      </header>
+          {weather.temperature}
+          <span>&#176;C</span>
+        </div>
+        <hr />
+        <div className="description">
+          {weather.description}
+          <img
+            alt="weather icon"
+            src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+          />
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
